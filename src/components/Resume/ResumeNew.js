88a96473ec627dde4fb9f2e06861aa5694a65ef3
Harshaiitk <venkataharshapedada9@gmail.com>
2024-11-05 12:42:48 -0600

    added New Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Particle from '../Particle'
-import pdf from '../../Assets/../Assets/harsha_resume .pdf'
+import pdf from '../../Assets/../Assets/Venkata_Harsha_Resume.pdf'
 import { AiOutlineDownload } from 'react-icons/ai'
 
 function ResumeNew() {
